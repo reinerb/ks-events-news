b95933ca509bea949e3440326bc304cf902c6c60
Ben Reiner <btr@btreiner.com>
2023-11-10 12:07:38 -0500

    Add find_featured_posts function

--- functions/find-posts.php
+++ functions/find-posts.php
@@ -5,6 +5,11 @@
  * @param array $query_params The query parameters
  * @return array An array of WordPress posts
  */
+
+use function PHPSTORM_META\map;
+
+require plugin_dir_path( __FILE__ ) . "classes/FeaturedPost.php";
+
 function find_posts(array $query_params) {
   $query = new WP_QUERY($query_params);
 
@@ -58,4 +63,36 @@ function find_news_posts(string $category_name = 'homepage-news', int $number_of
   ];
 
   return find_posts($query_params);
+}
+
+/**
+ * Returns an array of FeaturedPost objegts from the given category
+ * @param string $category_name The category to get posts from
+ * @param int $number_of_posts The number of posts to retrieve
+ * @return array FeaturedPost[] of all found posts
+ */
+function find_featured_posts(string $category_name = 'featured', int $number_of_posts = 4): array {
+  $query_params = [
+    'category_name' => $category_name,
+    'posts_per_page' => $number_of_posts,
+    'order' => 'DESC',
+    'orderby' => 'date'
+  ];
+
+  $featured_posts = array_map(function($post) {
+    $img_url = get_the_post_thumbnail_url($post, 'full');
+    $excerpt = get_the_excerpt($post);
+    $permalink = get_permalink($post);
+    $event_date = get_post_meta($post->ID,'event_date', true);
+
+    return new FeaturedPost(
+      $img_url,
+      $post->post_title,
+      $excerpt,
+      $permalink,
+      $event_date == '' ? null : $event_date
+    );
+  }, find_posts($query_params));
+  
+  return $featured_posts;
 }
