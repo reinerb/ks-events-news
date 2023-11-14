64f6f63b39a4cff19dac89e67fab6516a243b98e
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-11-14 10:43:46 -0500

    Clean up functions

--- functions/find-posts.php
+++ functions/find-posts.php
@@ -14,20 +14,3 @@ function find_posts(array $query_params) {
     throw new Exception("No posts found");
   }
 }
-
-/**
- * Gets the latest news posts.
- * @param string $category_name The slug of the category to search.
- * @param int $number_of_posts The number of posts to fetch
- * @return array An array of WordPress posts
- */
-function find_news_posts(string $category_name = 'homepage-news', int $number_of_posts = 3) {
-  $query_params = [
-    'category_name' => $category_name,
-    'posts_per_page' => $number_of_posts,
-    'order' => 'DESC', 
-    'orderby' => 'date'
-  ];
-
-  return find_posts($query_params);
-}
